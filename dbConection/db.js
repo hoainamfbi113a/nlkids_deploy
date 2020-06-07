7a1588fb9f140824402ab46d771038dcecb440eb
Nguyễn Hoài Nam <nguyenhoainam@172.31.99.123>
2020-06-07 10:49:58 +0700

    add google cloud

--- dbConection/db.js
+++ dbConection/db.js
@@ -1,6 +1,8 @@
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/NLkids', 
+const stringConection ="mongodb+srv://nlkids:nlkids@cluster0-gvkap.mongodb.net/<dbname>?retryWrites=true&w=majority"||
+"mongodb://localhost:27017/NLkids"
+mongoose.connect(stringConection, 
 { useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
